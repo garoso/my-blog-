c7c6c609925ce37f669fa73db44fc38b4440a6df
garoso <david.fd.251@gmail.com>
2018-10-23 17:07:04 -0500

    Index posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+    @posts = Post.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_190538) do
+ActiveRecord::Schema.define(version: 2018_10_23_200914) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
