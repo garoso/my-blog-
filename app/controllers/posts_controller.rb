e0309b5563d9269116f414dd500a5bb40f013811
garoso <david.fd.251@gmail.com>
2018-10-24 17:33:29 -0500

    Define current user as post owner

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,17 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path, notice: "El post se publico exitosamente"
+    else
+      render :new
+    end
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id )
+  end
 end
