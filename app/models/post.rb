8d0880b1cbf531f283ad2fc6c5f5cc5fe038046f
garoso <david.fd.251@gmail.com>
2018-10-31 16:03:12 -0500

    Create Comment functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  has_many :comments
+
   validates :title, :body, :user_id, presence: true
-  validates :body, length: {maximum: 250}
+  # validates :body, length: {maximum: 250}
 end
