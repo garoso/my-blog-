07b4de8b1a4eea4c5b03ffd69621fb08c5e5e6d9
garoso <david.fd.251@gmail.com>
2018-11-02 14:02:49 -0500

    Apply styles to devise flash messages, validate minimum 250 characteres for post body

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :title, :body, :user_id, presence: true
-  # validates :body, length: {maximum: 250}
+  validates :body, length: {minimum: 250}
 end
