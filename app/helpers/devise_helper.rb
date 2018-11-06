e05b490528103f33c590e9eb3a7124bcf74a5f0a
garoso <david.fd.251@gmail.com>
2018-11-06 17:50:05 -0500

    Delete sentence for resgistration error

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -10,13 +10,8 @@ module DeviseHelper
     return "" if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t("errors.messages.not_saved",
-                      count: resource.errors.count,
-                      resource: resource.class.model_name.human.downcase)
-
     html = <<-HTML
     <div id="error_explanation" class="alert alert-danger">
-      <!-- <h2>#{sentence}</h2> -->
       <ul>#{messages}</ul>
     </div>
     HTML
